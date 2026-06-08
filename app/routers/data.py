aad3b87673d28ebcb620ce2c33883a2dabeef002
Gauthier <argentieri.gauthier@gmail.com>
2026-06-08 07:41:01 +0200

    ras

--- app/routers/data.py
+++ app/routers/data.py
@@ -6,7 +6,7 @@ import json
 from datetime import date, timedelta
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query, Header
+from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel
 from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
